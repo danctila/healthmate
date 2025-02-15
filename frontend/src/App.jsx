866d05c49917b6474ca5e34a904e72e68de067cc
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-02-15 16:36:16 -0500

    feat: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import ContactDirectory from "./pages/ContactDirectory";
+import Home from "./pages/Home";
+import FindCare from "./pages/FindCare";
+import Account from "./pages/Account";
+import TestNav from "./components/TestNav";
 
 function App() {
   return (
     <>
-      <h1>Tester Boilerplate</h1>
+      <TestNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactDirectory />} />
+        <Route path="/care" element={<FindCare />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </>
   );
 }
