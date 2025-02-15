736233505ead70194eb768bc345116cafb2e4b59
jenishk20 <kotharijenish2001@gmail.com>
2025-02-15 17:50:38 -0500

    Setting Up TailwindCSS.

--- frontend/src/components/TestNav.jsx
+++ frontend/src/components/TestNav.jsx
@@ -3,26 +3,50 @@ import { Link } from "react-router-dom";
 
 const TestNav = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/care">Find Care</Link>
-        </li>
-        <li>
-          <Link to="/account">Account</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/auth">Auth</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    <div className="container mx-auto px-6 py-5">
+        <nav className="flex items-center justify-between bg-gradient-to-r from-blue-600 to-blue-800 text-white py-4 px-6 rounded-lg">
+          <Link to="/" className="flex items-center space-x-3">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"
+              alt="TradeHub Logo"
+              className="h-10 w-auto object-contain"
+            />
+            <span className="text-xl font-bold text-gray-800 hover:text-indigo-300 transition-colors duration-300">
+              HealthAI
+            </span>
+          </Link>
+          <ul
+          className={`lg:flex lg:items-center lg:justify-between lg:space-x-6 w-full lg:w-auto lg:flex-row flex flex-col lg:ml-auto mt-4 lg:mt-0`}
+        >
+            <li>
+              <Link
+                to="/"
+                className="text-gray-200 hover:text-indigo-300 transition-colors duration-300"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className="text-gray-200 hover:text-indigo-300 transition-colors duration-300"
+              >
+                Find Care
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="text-gray-200 hover:text-indigo-300 transition-colors duration-300"
+              >
+                Contact
+              </Link>
+            </li>
+            </ul>
+        </nav>
+
+    </div>
+  )
 };
 
 export default TestNav;
