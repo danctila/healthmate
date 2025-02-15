c530dd1bd7d4d25e5647311a8b2f145699709b33
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-02-15 16:28:55 -0500

    chore: base components

---
+++ frontend/src/pages/ContactDirectory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactDirectory = () => {
+  return <div>ContactDirectory</div>;
+};
+
+export default ContactDirectory;
---
+++ frontend/src/pages/FindCare.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FindCare = () => {
+  return <div>FindCare</div>;
+};
+
+export default FindCare;
